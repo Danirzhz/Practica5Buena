6f953865dce5f58b32418cd1591c53d4417b7b71
Danirzhz <danirzhz@gmail.com>
2022-04-11 20:28:22 +0200

    Initial commit

---
+++ app/src/main/java/net/iessochoa/danielruizhernandez/practica5buena/MainActivity.java
@@ -0,0 +1,14 @@
+package net.iessochoa.danielruizhernandez.practica5buena;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
